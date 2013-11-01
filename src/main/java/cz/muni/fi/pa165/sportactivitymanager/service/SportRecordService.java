03573b52be6a3461ac1c79c9757dd9fce1f53780
adambr <>
2013-11-01 20:46:31 +0100

    Adam Brauner last commit

---
+++ src/main/java/cz/muni/fi/pa165/sportactivitymanager/service/SportRecordService.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.sportactivitymanager.service;
+
+import cz.muni.fi.pa165.sportactivitymanager.SportRecord;
+import java.util.List;
+
+/**
+ *
+ * @author Phaser
+ */
+public class SportRecordService{
+
+    public void create(SportRecord sportRecord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public SportRecord getSportRecord(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete(SportRecord sportRecord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void update(SportRecord sportRecord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<SportRecord> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
